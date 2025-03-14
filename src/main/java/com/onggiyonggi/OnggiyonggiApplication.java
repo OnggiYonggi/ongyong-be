bf60e74227a3bb1f65c694046d2cd8beb0ce5e2e
mingmingmon <mingmingmon@naver.com>
2025-03-14 20:38:45 +0900

    setting: 스프링부트 프로젝트 세팅 완료 #1

---
+++ src/main/java/com/onggiyonggi/OnggiyonggiApplication.java
@@ -0,0 +1,13 @@
+package com.onggiyonggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnggiyonggiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnggiyonggiApplication.class, args);
+    }
+
+}
