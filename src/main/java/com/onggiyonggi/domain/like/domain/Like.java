d0522f386cd059f918554038c9ce6d60c52ccf93
mingmingmon <mingmingmon@naver.com>
2025-04-07 15:41:49 +0900

    feat: 품목, 좋아요 엔티티 구현 완료 #23

---
+++ src/main/java/com/onggiyonggi/domain/like/domain/Like.java
@@ -0,0 +1,39 @@
+package com.onggiyonggi.domain.like.domain;
+
+import com.onggiyonggi.domain.member.domain.Member;
+import com.onggiyonggi.domain.review.domain.Review;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "\"Like\"")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
